11a9a0fa92bc2293b6cda64f5c46854887a76feb
Gabriel Sanches <gabriel.sanches1803@gmail.com>
2025-02-11 09:34:06 -0300

    Home page finished

--- src/componentes/Body/advantages/index.js
+++ src/componentes/Body/advantages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled from 'styled-components';
+import { PiGearSixDuotone, PiStarDuotone, PiFactoryDuotone  } from "react-icons/pi";
 
 const Container = styled.div`
   background-color: #073e7c ;
   color: white;
   width: 100vw;
   padding: 20px;
+  padding-top: 50px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -19,6 +21,8 @@ const IconContainer = styled.div`
   width: 100%;
   max-width: 1200px; // Define uma largura máxima para o conteúdo
   margin: 0 auto; // Centraliza o conteúdo
+  margin-top: 25px;
+  margin-bottom: 50px;
 
   @media (max-width: 768px) {
     flex-direction: column;
@@ -32,30 +36,40 @@ const IconWrapper = styled.div`
   flex: 1;
 `;
 
+const Icone = styled.a`
+  transform: scale(3); /* Aumenta a escala do ícone em 1.5 vezes */
+  display: flex; /* Garante que o ícone seja dimensionado corretamente */
+  justify-content: center; /* Centraliza o ícone horizontalmente */
+  align-items: center; /* Centraliza o ícone verticalmente */
+  margin-bottom: 25px;
+  margin-top: 25px;
+  color: white;
+`
+const Paragrafo = styled.p`
+  margin: 0; /* Remove a margem entre os parágrafos */
+  line-height: 1.5; /* Ajusta a altura da linha */
+`
+
 function Advantages() {
-    return (
-        <Container>
-            <h2>Diferenciais</h2>
-            <IconContainer>
-                <IconWrapper>
-                    <img src="icon1.png" alt="Desenvolvemos os artefatos de borracha sob medida" className="img-fluid" />
-                    <p>Desenvolvemos os artefatos de borracha sob medida, em conjunto com o departamento de engenharia do cliente, para que os produtos atinjam as suas especificações de funcionalidade, produtividade e custo.</p>
-                </IconWrapper>
-                <IconWrapper>
-                    <img src="icon2.png" alt="Formulamos e produzimos a borracha internamente" className="img-fluid" />
-                    <p>Formulamos e produzimos a borracha internamente, garantindo a qualidade da matéria prima e assegurando que o cliente receba exatamente o material que pediu, com as características que necessita.</p>
-                </IconWrapper>
-                <IconWrapper>
-                    <img src="icon3.png" alt="Estamos localizados em Curitiba" className="img-fluid" />
-                    <p>Estamos localizados em Curitiba, contando com facilidade logística e experiência de fornecimento para todo o território nacional e América Latina.</p>
-                </IconWrapper>
-                <IconWrapper>
-                    <img src="icon4.png" alt="Contamos com diferentes métodos de produção" className="img-fluid" />
-                    <p>Contamos com diferentes métodos de produção, podendo fornecer toda quantidade de produtos - de poucas unidades a milhões de peças ao mês -, destacando-nos na injeção de produtos de borracha e silicone, bem como na fabricação de perfis e mangueiras dos mesmos materiais.</p>
-                </IconWrapper>
-            </IconContainer>
-        </Container>
-    );
+  return (
+    <Container>
+      <h2>Vantagens</h2>
+      <IconContainer>
+        <IconWrapper>
+          <Icone><PiGearSixDuotone /></Icone>
+          <Paragrafo>Produzimos artefatos de borracha personalizados para garantir que os produtos atendam perfeitamente às suas especificações de funcionalidade, eficiência e custo.</Paragrafo>
+        </IconWrapper>
+        <IconWrapper>
+          <Icone><PiStarDuotone /></Icone>
+          <Paragrafo>Realizamos a formulação e produção interna da borracha, assegurando a qualidade da matéria-prima com as especificações necessárias, garantindo a qualidade exigida por nossos clientes.</Paragrafo>
+        </IconWrapper>
+        <IconWrapper>
+          <Icone><PiFactoryDuotone  /></Icone>
+          <Paragrafo>Utilizamos diversos métodos de produção para fornecer desde poucas unidades até milhões de peças, especializados em injeção de borracha e silicone.</Paragrafo>
+        </IconWrapper>
+      </IconContainer>
+    </Container>
+  );
 };
 
 export default Advantages;
